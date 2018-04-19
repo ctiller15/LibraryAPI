8b283abbc35999d783688ac555e4215bbd1a206f
Tiller <ctiller@proforma.com>
2018-04-19 14:03:14 -0400

    install Entityframework and create LibraryContext

---
+++ LibraryAPI/DbContext/LibraryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAPI.Data
+{
+    public class LibraryContext:DbContext
+    {
+        public MoviesContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+    }
+}
