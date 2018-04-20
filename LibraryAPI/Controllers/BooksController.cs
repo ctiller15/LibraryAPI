030dd04581912d5253b236b59fd718e5dfd4ccf5
Tiller <ctiller@proforma.com>
2018-04-20 10:17:28 -0400

    Get author check working correctly w/ new and old authors

--- LibraryAPI/Controllers/BooksController.cs
+++ LibraryAPI/Controllers/BooksController.cs
@@ -14,6 +14,35 @@ namespace LibraryAPI.Controllers
 {
     public class BooksController : ApiController
     {
+        static Author CheckAuthor(PostBook book)
+        {
+            Author author = null;
+            var db = new LibraryContext();
+            if (!String.IsNullOrEmpty(book.AuthorName))
+            {
+                string authorName = book.AuthorName;
+
+                var match = db.Authors.FirstOrDefault(x => x.Name == book.AuthorName);
+
+                if (match != null)
+                {
+                    // Reference match in DB, set it to author.
+                    author = match;
+                }
+                else
+                {
+                    // Create author.
+                    author = new Author
+                    {
+                        Name = book.AuthorName
+                    };
+                    db.Authors.Add(author);
+                    db.SaveChanges();
+                }
+            }
+            return db.Authors.FirstOrDefault(x => x.Name == book.AuthorName);
+        }
+
         // GET: Find a book based on title, author, or genre
         public IEnumerable<Book> Get([FromUri]GetBook book)
         {
@@ -26,6 +55,7 @@ namespace LibraryAPI.Controllers
                     query = query.Where(w => w.Title == book.BookTitle);
                 }
 
+                // TODO: Currently works with just Title. Get working w/ Author and Genre!
                 if (!String.IsNullOrEmpty(book.BookAuthor))
                 {
                     query = query.Where(w => w.Author.Name == book.BookAuthor);
@@ -43,12 +73,23 @@ namespace LibraryAPI.Controllers
         // POST: Add a new Book
         public IHttpActionResult Post(PostBook book)
         {
+            Author author = CheckAuthor(book);
+
             var newBook = new Book
             {
                 Title = book.BookTitle,
                 YearPublished = book.YearPublished,
                 Condition = book.Condition,
                 // Don't worry about Author right now...
+
+                // Get Author name.
+                // Check if it currently exists in db.
+                // If it exists in db, just grab the old one.
+                // Otherwise, create it.
+
+                AuthorID = author.ID,
+                //Author = author,
+
                 // Don't worry about Genre right now...
                 ISBN = book.ISBN,
                 IsCheckedOut = book.IsCheckedOut,
@@ -58,6 +99,7 @@ namespace LibraryAPI.Controllers
             var db = new LibraryContext();
             db.Books.Add(newBook);
             db.SaveChanges();
+            newBook.Author = author;
             return Ok(newBook);
         }
 
